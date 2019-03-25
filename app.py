43bcdc4c26aaf09d646dc4bd94252022aa6da711
qweaper <reaperzaz@gmai.com>
2019-03-25 22:11:00 +0300

    Добавлены: -главная страница без разметки -раздел загрузки файлов -БД адаптированно под работу приложения -соответствующие HTML страницы для разделов

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, redirect, render_template, request
 from userDB import UserModel
-from booksDB import NewsModel  #######
+from database import DB
+from booksDB import BooksModel  #######
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, FileField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['pdf', 'txt', 'f2b'])
+db = DB()
+
+users = UserModel(db.get_connection())
+users.init_table()
+
+books = BooksModel(db.get_connection())
+books.init_table()
+try:
+    users.insert('admin', 'admin')
+except Exception:
+    pass
 session = {}
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 curr_user = ''
 
 
-class AddNewsForm(FlaskForm):
-    title = StringField('Заголовок новости', validators=[DataRequired()])
-    content = TextAreaField('Текст новости', validators=[DataRequired()])
+class AddNewBook(FlaskForm):
+    title = StringField('Название книги', validators=[DataRequired()])
+    content = TextAreaField('Описание книги', validators=[DataRequired()])
+    file = FileField('Добавить книгу', validators=[DataRequired()])
     submit = SubmitField('Добавить')
 
 
@@ -32,8 +46,14 @@ class SignInForm(FlaskForm):
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    if 'username' not in session:
+        return redirect('/login')
+    mod = 0
+    if session['username'] == 'admin':
+        mod = 1
+    return render_template('base.html', mod=mod, username=session['username'])
 
 
 @app.route('/success')
@@ -68,26 +88,54 @@ def sign_in():
         return redirect('/error')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if request.method == 'GET':
+        return render_template('login.html', title='Авторизация', form=form)
+    if request.method == 'POST':
+        try:
+            user_name = form.username.data
+            password = form.password.data
+            user_model = UserModel(db.get_connection())
+            exists = user_model.exists(user_name, password)
+            if exists[0]:
+                session['username'] = user_name
+                session['user_id'] = exists[1]
+            return redirect("/index")
+        except ValueError:
+            return redirect('/error')
+
+
+@app.route('/download_file', methods=['POST', 'GET'])
+def sample_file_upload():
+    form = AddNewBook()
+    if request.method == 'GET':
+        return render_template('download_file.html', form=form, username=session['username'])
+    elif request.method == 'POST':
+        try:
+            f = request.files['file']
+            if f.filename[f.filname.index('.') in ALLOWED_EXTENSIONS]:
+                with open('books_files/' + f.filename, 'wb') as file:
+                    data = f.read()
+                    file.write(data)
+                return redirect('/')
+            else:
+                return redirect('/error')
+        except Exception:
+            return redirect('/error')
+
+
 @app.route('/users_log')
 def users_log():
     user_list = [i[1] for i in users.get_users()]
     logs = {}
     for user in list(set(user_list)):
         us = users.get_id(user)[1]
-        logs[user] = len(news.get_all(us))
-    return render_template('user_logs.html', logs=logs, user_list=user_list)
+        logs[user] = len(books.get_all(us))
+    return render_template('user_logs.html', logs=logs, user_list=user_list, username='admin')
 
 
 if __name__ == '__main__':
-    db = DB()
-
-    users = UserModel(db.get_connection())
-    users.init_table()
-
-    news = NewsModel(db.get_connection())
-    news.init_table()
-    try:
-        users.insert('admin', 'admin')
-    except Exception:
-        pass
     app.run(port=8080, host='127.0.0.1', debug=True)
--- booksDB.py
+++ booksDB.py
@@ -1,46 +1,46 @@
-class NewsModel:
+class BooksModel:
 
     def __init__(self, connection):
         self.connection = connection
 
     def init_table(self):
         cursor = self.connection.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS news 
+        cursor.execute('''CREATE TABLE IF NOT EXISTS books 
                             (id INTEGER PRIMARY KEY AUTOINCREMENT, 
                              title VARCHAR(100),
-                             content VARCHAR(1000),
-                             user_id INTEGER,
-                             date_time TIMESTAMP 
+                             description VARCHAR(1000),
+                             book_filename VARCHAR (50),
+                             user_id INTEGER
                              )''')
         cursor.close()
         self.connection.commit()
 
-    def insert(self, title, content, userid, time):
+    def insert(self, title, description, user_id, book_filename):
         cursor = self.connection.cursor()
-        cursor.execute('''INSERT INTO news 
-                          (title, content, user_id, date_time) 
-                          VALUES (?,?,?,?)''', (title, content, str(userid), time))
+        cursor.execute('''INSERT INTO books
+                          (title, description, user_id, book_filename) 
+                          VALUES (?,?,?,?)''', (title, description, str(user_id), book_filename))
         cursor.close()
         self.connection.commit()
 
-    def get(self, news_id):
+    def get(self, book_id):
         cursor = self.connection.cursor()
-        cursor.execute("SELECT * FROM news WHERE id = ?", (str(news_id)))
+        cursor.execute("SELECT * FROM books WHERE id = ?", (str(book_id)))
         row = cursor.fetchone()
         return row
 
     def get_all(self, user_id=None):
         cursor = self.connection.cursor()
         if user_id:
-            cursor.execute("SELECT * FROM news WHERE user_id = ? ORDER BY date_time DESC",
+            cursor.execute("SELECT * FROM books WHERE user_id = ?",
                            (str(user_id)))
         else:
-            cursor.execute("SELECT * FROM news ORDER BY date_time DESC")
+            cursor.execute("SELECT * FROM books")
         rows = cursor.fetchall()
         return rows
 
-    def delete(self, news_id):
+    def delete(self, book_id):
         cursor = self.connection.cursor()
-        cursor.execute('''DELETE FROM news WHERE id = ?''', (str(news_id)))
+        cursor.execute('''DELETE FROM books WHERE id = ?''', (str(book_id)))
         cursor.close()
         self.connection.commit()
