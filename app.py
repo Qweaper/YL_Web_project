a138ef095c3f860d5011453261b52b9c9c48ccb7
qweaper <reaperzaz@gmai.com>
2019-03-30 01:05:24 +0300

    Добавлены функции: -скачивания файла -перемещения книг между списками -информация в библиотеке о находжении книги в списке -добавление книги из библиотеки в список на выбор

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import hashlib
 import json
-from flask import Flask, redirect, render_template, request
-from userDB import UserModel
-from database import DB
-from booksDB import BooksModel
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+
+from flask import Flask, redirect, render_template, request, send_from_directory
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField
 from wtforms import StringField, SubmitField, TextAreaField, FileField
 from wtforms.validators import DataRequired
-from flask_wtf import FlaskForm
+
+from booksDB import BooksModel
+from database import DB
+from userDB import UserModel
 
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['pdf', 'txt', 'f2b'])
@@ -18,18 +21,19 @@ users.init_table()
 books = BooksModel(db.get_connection())
 books.init_table()
 try:
-    users.insert('admin', 'admin')
+    users.insert('admin', '21232f297a57a5a743894a0e4a801fc3')
 except Exception:
     pass
 session = {}
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 curr_user = ''
+FILE_DIR = 'books_files'
 
 
 def get_booklist_of_user(user_id, list_type):
     with open(list_type, 'r') as lst:
         data = json.loads(lst.read())
-        return data[str(user_id)]
+        return set(data[str(user_id)])
 
 
 def remove_book_from_list(book_id, user_id, list_type):
@@ -41,13 +45,23 @@ def remove_book_from_list(book_id, user_id, list_type):
             datawrite.write(json.dumps(lst))
 
 
-def add_user_to_list(book_id, user_id, list_type):
+def add_book_to_list(book_id, user_id, list_type):
     with open(list_type, 'r') as data:
         lst = json.loads(data.read())
-
         with open(list_type, 'w') as datawrite:
-            lst[str(user_id)] = lst.get(str(user_id), []) + [book_id]
-            datawrite.write(json.dumps(lst))
+            if book_id not in lst[str(user_id)]:
+                lst[str(user_id)] = lst.get(str(user_id), []) + [book_id]
+                datawrite.write(json.dumps(lst))
+
+
+def check_book_list(user_id, book_id):
+    words = {'wishlist': 'желаний', 'readinglist': 'читаемого', 'hreadlist': 'прочитанного'}
+    for i in ['wishlist', 'readinglist', 'hreadlist']:
+        with open(i + '.json', 'r') as file:
+            data = json.loads(file.read())
+            if book_id in data[str(user_id)]:
+                return words[i]
+    return False
 
 
 class AddNewBook(FlaskForm):
@@ -119,10 +133,10 @@ def logout():
 def lists():
     if 'username' not in session:
         return redirect('/login')
-    link = request.path
+    link = request.path[1:]
     try:
         if request.method == 'GET':
-            wishlist = get_booklist_of_user(session['user_id'], link[1:] + '.json')
+            wishlist = get_booklist_of_user(session['user_id'], link + '.json')
             for i in wishlist:
                 book_info = books.get(i)
             return render_template('wishlist.html', username=session['username'], booklist=book_info, path=link)
@@ -140,7 +154,9 @@ def sign_in():
             return render_template('sign_in.html', form=form)
         if request.method == 'POST':
             user_name = form.username.data
-            password = form.password.data
+            hash_of_passw = hashlib.md5(form.password.data.encode('utf-8'))
+            password = hash_of_passw.hexdigest()
+            print(password)
             users.insert(user_name, password)
             return redirect('/login')
     except ValueError:
@@ -156,12 +172,15 @@ def login():
     if request.method == 'POST':
         try:
             user_name = form.username.data
-            password = form.password.data
+            hash_of_passw = hashlib.md5(form.password.data.encode('utf-8'))
+            password = hash_of_passw.hexdigest()
+            print(password)
             user_model = UserModel(db.get_connection())
             exists = user_model.exists(user_name, password)
             if exists[0]:
                 session['username'] = user_name
                 session['user_id'] = exists[1]
+
             return redirect("/index")
         except ValueError:
             return redirect('/error')
@@ -180,30 +199,64 @@ def sample_file_upload():
 
         if f.filename[f.filename.index('.') in ALLOWED_EXTENSIONS]:
             books.insert(form.author._value(), form.title._value(), form.description._value(), session['user_id'],
-                         request.files['file'])
+                         request.files['file'].filename)
 
-            book_id = books.get_book_id(form.title._value(), form.description._value(), session['user_id'],
-                                        request.files['file'])[0]
+        book_id = books.get_book_id(form.title._value(), form.description._value(), session['user_id'],
+                                    request.files['file'].filename)[0]
 
-            add_user_to_list(book_id, session['user_id'], 'readinglist.json')
+        add_book_to_list(book_id, session['user_id'], 'readinglist.json')
 
-            users.increase_num_of_books(session['user_id'])
+        users.increase_num_of_books(session['user_id'])
 
-            with open('books_files/' + f.filename, 'wb') as file:
-                data = f.read()
-                file.write(data)
+        with open('books_files/' + f.filename, 'wb') as file:
+            data = f.read()
+            file.write(data)
 
-            return redirect('/')
-        else:
-            return '''Wrong extension of the file'''
-    # except Exception:
-    #     return redirect('/error')
+        return redirect('/')
+    else:
+        return '''Wrong extension of the file'''
+# except Exception:
+#     return redirect('/error')
 
 
 @app.route('/delete/<string:path>/<int:book_id>')
 def delete(path, book_id):
+    print(path)
     remove_book_from_list(book_id, session['user_id'], path + '.json')
     return redirect('/' + path)
+
+
+# добавить функцию, говорящую о расположение книги в списке
+@app.route('/library')
+def library():
+    booklist = books.get_all()
+    # exceptions = books.get_all(session['user_id'])
+    for i in booklist:
+        lst = [list(i) + [check_book_list(session['user_id'], i[0])]]
+    return render_template('library.html', username=session['username'], booklist=lst)
+
+
+@app.route('/add_to_hread/<int:book_id>')
+@app.route('/add_to_reading/<int:book_id>')
+@app.route('/add_to_wishes/<int:book_id>')
+def add_book(book_id):
+    path = request.path[8:-2]
+    print(path)
+    add_book_to_list(book_id, session['user_id'], path + 'list.json')
+    return redirect('/library')
+
+
+@app.route('/change/<string:frm>/<string:to>/<int:book_id>')
+def change_book_location(book_id, frm, to):
+    add_book_to_list(book_id, session['user_id'], to + '.json')
+    remove_book_from_list(book_id, session['user_id'], frm + '.json')
+    return redirect('/' + frm)
+
+
+@app.route('/download/<int:book_id>')
+def download(book_id):
+    filename = books.get(book_id)[0][-3]
+    return send_from_directory(FILE_DIR, filename)
 
 
 @app.route('/users_log')
@@ -220,4 +273,5 @@ def users_log():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1', debug=True)
+    app.run(port=8080, host='0.0.0.0', debug=True)
+    # 0.0.0.0 можно будет смотреть через разные устройства
